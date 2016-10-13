5fd8367d2ee5abdf4bdab5b10902cfd77e666ee9
pracdev <davidprod210@gmail.com>
2016-10-13 14:22:25 +0200

    Changes with user config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,9 @@
 # Don't change these unless you know what you're doing
 set :pty,             true
-set :use_sudo,        false
+set :use_sudo,        true
 set :stage,           :production
 set :deploy_via,      :remote_cache
+set :user,            'deployer'
 
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
--- config/deploy/hello/production.rb
+++ config/deploy/hello/production.rb
@@ -2,19 +2,19 @@ server 'mobizard.com', port: 22, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:pracdev/testing-deploy.git'
 set :application,     'testing-deploy'
-set :user,            'deployer'
+# set :user,            'deployer'
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :linked_files, %w{config/database.yml config/application.yml .env}
 set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 set :branch, 'master' # ask :branch, 'master'
 set :foreman_export_path, "/home/deployer/#{fetch(:application)}/shared/init"
 
-after 'deploy:updated', 'deploy:compile_assets'
-after 'deploy:updated', 'deploy:normalize_assets'
-after 'deploy:reverted', 'deploy:rollback_assets'
+# after 'deploy:updated', 'deploy:compile_assets'
+# after 'deploy:updated', 'deploy:normalize_assets'
+# after 'deploy:reverted', 'deploy:rollback_assets'
 
-after 'deploy:published', 'deploy:migrate'
-after 'deploy:finished', 'deploy:log_revision'
+# after 'deploy:published', 'deploy:migrate'
+# after 'deploy:finished', 'deploy:log_revision'
 
 #before 'deploy:publishing', 'foreman:export'
 
