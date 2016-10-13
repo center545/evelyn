2b6a4910aeffaa73762a0c330329dc5ffe5078e7
pracdev <davidprod210@gmail.com>
2016-10-13 18:07:27 +0200

    Changes destination directory

--- config/deploy/hello/production.rb
+++ config/deploy/hello/production.rb
@@ -3,11 +3,11 @@ server 'mobizard.com', port: 22, roles: [:web, :app, :db], primary: true
 set :repo_url,        'git@github.com:pracdev/testing-deploy.git'
 set :application,     'testing-deploy'
 # set :user,            'deployer'
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :deploy_to,       "/home2/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :linked_files, %w{config/database.yml config/application.yml .env}
 set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 set :branch, 'master' # ask :branch, 'master'
-set :foreman_export_path, "/home/deployer/#{fetch(:application)}/shared/init"
+set :foreman_export_path, "/home2/deployer/#{fetch(:application)}/shared/init"
 
 # after 'deploy:updated', 'deploy:compile_assets'
 # after 'deploy:updated', 'deploy:normalize_assets'
