01fa3072f7d526ff12d08ca11a1c521e0067ee21
pracdev <davidprod210@gmail.com>
2016-10-14 14:33:34 +0200

    Removed .env from deploy script

--- config/deploy/hello/production.rb
+++ config/deploy/hello/production.rb
@@ -4,7 +4,7 @@ set :repo_url,        'git@github.com:pracdev/testing-deploy.git'
 set :application,     'testing-deploy'
 # set :user,            'deployer'
 set :deploy_to,       "/home2/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :linked_files, %w{config/database.yml config/application.yml .env}
+set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 set :branch, 'master' # ask :branch, 'master'
 set :foreman_export_path, "/home2/deployer/#{fetch(:application)}/shared/init"
